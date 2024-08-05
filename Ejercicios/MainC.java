8496d47d820918f05920847894a73a947e020903
Antoniocr7 <cortezantonio124@gmail.com>
2024-08-05 12:27:07 -0400

    Delete Ejercicios/MainC.java

--- Ejercicios/MainC.java
+++
@@ -1,8 +0,0 @@
-public class MainC {
-    public static void main(String[] args) {
-        Contador c1 = new Contador();
-        Contador c2 = new Contador();
-        Contador c3 = new Contador();
-        System.out.println(Contador.getCount());
-    }
-}
