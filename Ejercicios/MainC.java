1c17eff7c6b77142e326f50c4e86f8d1d9fa03e6
Antoniocr7 <cortezantonio124@gmail.com>
2024-08-05 17:00:18 +0000

    Ejercicio: #23

---
+++ Ejercicios/MainC.java
@@ -0,0 +1,8 @@
+public class MainC {
+    public static void main(String[] args) {
+        Contador c1 = new Contador();
+        Contador c2 = new Contador();
+        Contador c3 = new Contador();
+        System.out.println(Contador.getCount());
+    }
+}
